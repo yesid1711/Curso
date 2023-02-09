eb364747a87912be068cdaeb45fe705c025f8525
yesid <yevillamizarto@gmail.com>
2023-02-09 12:50:22 -0500

    commit inicial

---
+++ src/Calificacion.js
@@ -0,0 +1,6 @@
+const calificacion = 3;
+if (calificacion >= 3){
+    console.log ('Aprobo');
+} else{
+console.log('Reprobo');
+    }
---
+++ src/resta.js
@@ -0,0 +1,7 @@
+function resta (a,b){
+var aux = a-b;
+return total = aux;
+ 
+}
+var dato1 = resta(3,8);
+console.log(dato1);
