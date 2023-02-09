3afc024f2b3ab0f43a5e1435a0b766ff7e82cf15
yesid <yevillamizarto@gmail.com>
2023-02-09 14:05:52 -0500

    nnuevo cambio

---
+++ Proyecto/index.js
@@ -0,0 +1,10 @@
+const fn = () => {
+    const miString = " Soy una Cadena String";
+    let b = miString +" Muy Feliz";
+    console.log (b);
+}
+fn()
+const clickeado = () => {
+    let a = " Aprendiz Feliz";
+    console.log (a);
+}
